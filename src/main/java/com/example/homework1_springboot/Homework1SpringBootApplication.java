3aa92806a2971a12e9e58560e6af20e1a737f114
Vereshchagin_Mikhail <farasers@mail.ru>
2022-12-08 20:35:56 +0300

    ДЗ №1 SpringBoot

---
+++ src/main/java/com/example/homework1_springboot/Homework1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework1SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework1_springboot/Homework1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework1_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework1SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
